f5952ed5a3618cf177830e425482919914aca4d3
agizzi <alexandra.n.gizzi@gmail.com>
2019-10-09 20:19:20 -0400

    minor format changes, same amount of broken

--- mystery.py
+++ mystery.py
@@ -30,7 +30,7 @@ def end_game():
             # print("Wow! You win! Press the up arrow and hit enter to play again. :) ")
             for letter in guess:
                 board2.append(letter)
-                board = board2
+            board = board2
             print("".join(board))
             return board
         else:
@@ -38,25 +38,26 @@ def end_game():
             guess = input("Guess a letter or word :)  ")
 
     while (game_over == False):
-        if len(letter_guesses) >= 8:
-            print("GAME OVER LOSERRRR")
-            return
-        elif "".join(board) == answer:
+        if "".join(board) == answer:
             print("YOU WIN MOFO")
             return
         else:
-            guess = input("Guess a letter or word FUCKKKKK:)  ")
-            print("Incorrect guesses:", ", ".join(letter_guesses))
-            print("You have", 8-len(letter_guesses), "guesses remaining.")
-            if len(guess) == 1 and guess.isalpha():
-                compare_letter(answer, board, guess)
-                print(" ".join(board))
-                # guess = input("Guess a letter or word :)  ")
-            elif len(guess) == len(answer) and guess.isalpha():
-                compare_word(answer, board, guess)
+            if len(letter_guesses) >= 8:
+                print("GAME OVER LOSERRRR")
+                return
             else:
-                print("Oops, that's not a valid guess. :/ Try again. ")
-                # guess = input("Guess a letter or word :)  ")
+                guess = input("Guess a letter or word :)  ")
+                print("Incorrect guesses:", ", ".join(letter_guesses))
+                print("You have", 8-len(letter_guesses), "guesses remaining.")
+                if len(guess) == 1 and guess.isalpha():
+                    compare_letter(answer, board, guess)
+                    print(" ".join(board))
+                    # guess = input("Guess a letter or word :)  ")
+                elif len(guess) == len(answer) and guess.isalpha():
+                    compare_word(answer, board, guess)
+                else:
+                    print("Oops, that's not a valid guess. :/ Try again. ")
+                    # guess = input("Guess a letter or word :)  ")
 
 
 end_game()
